04dce6c17e95129bcfa05875b959106575bd7a20
Morgan <109176008+morgs999@users.noreply.github.com>
2024-08-26 14:30:36 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,17 +4,12 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import MemoryOutlinedIcon from '@mui/icons-material/MemoryOutlined';
-import BookOutlinedIcon from '@mui/icons-material/BookOutlined';
-import BrowserUpdatedOutlinedIcon from '@mui/icons-material/BrowserUpdatedOutlined';
-import ComputerOutlinedIcon from '@mui/icons-material/ComputerOutlined';
 import DesktopMacOutlinedIcon from '@mui/icons-material/DesktopMacOutlined';
-import DvrOutlinedIcon from '@mui/icons-material/DvrOutlined';
 import ReceiptLongOutlinedIcon from '@mui/icons-material/ReceiptLongOutlined';
 import PhoneIphoneOutlinedIcon from '@mui/icons-material/PhoneIphoneOutlined';
-import { FaBlackTie } from 'react-icons/fa';
 
 import { createTheme } from '@mui/material/styles';
+import { Grid } from '@mui/material';
 
 
 const theme = createTheme({
@@ -32,54 +27,56 @@ const theme = createTheme({
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1, mb: 20 }}>
-      <AppBar className='p-4' color='primary'  >
-        <Toolbar sx={{ color: theme.palette.primary.main }}>
+      <AppBar className='p-4' >
+        <Grid container justifyContent="flex-end">
+          <Toolbar>
 
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="home"
-            sx={{ mr: 2 }}
-            href='/'
-          >
-            <HomeOutlinedIcon />
-          </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="home"
+              sx={{ mr: 2, color: theme.palette.primary.main }}
+              href='/'
+            >
+              <HomeOutlinedIcon />
+            </IconButton>
 
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="portfolio"
-            sx={{ mr: 2 }}
-            href='/portfolio'
-          >
-            <DesktopMacOutlinedIcon />
-          </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="portfolio"
+              sx={{ mr: 2 }}
+              href='/portfolio'
+            >
+              <DesktopMacOutlinedIcon />
+            </IconButton>
 
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="resume"
-            sx={{ mr: 2 }}
-            href='/resume'
-          >
-            <ReceiptLongOutlinedIcon />
-          </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="resume"
+              sx={{ mr: 2 }}
+              href='/resume'
+            >
+              <ReceiptLongOutlinedIcon />
+            </IconButton>
 
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="contact"
-            sx={{ mr: 2 }}
-            href='/contact'
-          >
-            <PhoneIphoneOutlinedIcon />
-          </IconButton>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="contact"
+              sx={{ mr: 2 }}
+              href='/contact'
+            >
+              <PhoneIphoneOutlinedIcon />
+            </IconButton>
 
-        </Toolbar>
+          </Toolbar>
+        </Grid>
       </AppBar>
     </Box>
   );
