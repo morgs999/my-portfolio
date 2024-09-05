b35ab4b70a2d5c9d9b077b192810d0d8232bad4d
Morgan <109176008+morgs999@users.noreply.github.com>
2024-09-05 13:19:56 -0400

    added pdf download

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,12 @@
+
+
 export default function Resume() {
     return (
-        <h1>Resume</h1>
+        <>
+            <h1>Download My Resume</h1>
+            <a href='public/pdfs/resume.pdf' download='resume.pdf'>
+                <img src='public/pngs/resume.png' alt='resume' className='border rounded-4 shadow-lg p-5 m-5' />
+            </a>
+        </>
     )
 }
