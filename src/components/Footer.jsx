22989c1099d855fd51e4dc79613418d68c50296b
Morgan <109176008+morgs999@users.noreply.github.com>
2024-09-06 17:14:07 -0400

    footer
    added icons, links,  and small hover animation to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,22 @@
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LibraryMusicIcon from '@mui/icons-material/LibraryMusic';
+
 export default function Footer() {
     return (
-        <div>
+        <div className='bg-black pb-3'>
             <hr></hr>
-            Footer
+            <div container className='d-flex justify-content-center'>
+                <a href='https://linkedin.com/in/morgandclarke' target='_blank' rel='noreferrer'>
+                    <LinkedInIcon id='footerIcon' className='text-white m-1' fontSize='large' />
+                </a>
+                <a href='https://github.com/morgs999' target='_blank' rel='noreferrer'>
+                    <GitHubIcon id='footerIcon' className='text-white m-1' fontSize='large' />
+                </a>
+                <a href='https://open.spotify.com/artist/3B9orqUJOAa42fvR0TqtP5?si=5TZtj9bgTeiCyTmJxiflfQ' target='_blank' rel='noreferrer'>
+                    <LibraryMusicIcon id='footerIcon' className='text-white m-1' fontSize='large' />
+                </a>
+            </div>
         </div>
     )
 };
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,11 +2,11 @@ export default function Resume() {
     return (
         <>
             <div id='resume' className="container p-5">
-                <a href='/pdfs/resume.pdf' download='resume.pdf'>
+                <a href='/pdfs/Morgan_Clarke_Resume.pdf' download='Morgan_Clarke_Resume.pdf'>
+                    <h3 id="resumeText">Download My Resume</h3>
                     <img id='resumePic' src='/pngs/resume.png' alt='Morgan Clarke Resume' className='border rounded-4 shadow-lg p-5 m-5' />
                 </a>
-                <h3 id="resumeText">Download My Resume</h3>
             </div>
         </>
     )
-}
+};
