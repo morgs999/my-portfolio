08a2e6606c9df42f9f8e3d4ba986e58193cc0313
Morgan <109176008+morgs999@users.noreply.github.com>
2025-03-17 16:46:35 -0400

    Fixed About and Resume pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,37 +3,43 @@ import 'animate.css';
 import "animate.css/animate.compat.css"
 import ScrollAnimation from 'react-animate-on-scroll';
 import Portfolio from './Portfolio';
+import { Typography } from '@mui/material';
 
 export default function About() {
     return (
         <>
-            <div className="container intro-container p-5">
-                {/* <h1 className="signature" id=''>Morgan Clarke</h1> */}
+            <div className="container intro-container pt-5 d-flex flex-column justify-content-center align-items-center">
 
                 <h3 id="intro-1">
-                    I am a Full Stack Software Developer with experience in extensive Front End and Back End languages, frameworks, and libraries.  I create elegant yet powerful code that utilizes cutting edge tools to deploy intuitive software applications.
+                    <Typography variant='h5' className=''>
+                        I am a Full Stack Software Developer with experience in extensive Front End and Back End languages, frameworks, and libraries.  I create elegant yet powerful code that utilizes cutting edge tools to deploy intuitive software applications.
+                    </Typography>
                 </h3>
 
                 <h5 id='intro-2'>
-                    Having worked as an audio engineer for the last decade plus, I am fascinated by the inventive component of coding; in conjunction, I am also methodical in leveraging the necessary tools to solve tricky problems.
+                    <Typography variant='h6' className=' mt-2 p-3'>
+                        Having worked as an audio engineer for the last decade plus, I am fascinated by the inventive component of coding; in conjunction, I am also methodical in leveraging the necessary tools to solve tricky problems.
+                    </Typography>
                 </h5>
 
-                <ul id='intro-ul'>
-                    This extensive experience combining creative solutions with effective communication and organization allows me to:
+                <ul id='intro-ul' className='list-unstyled'>
+                    <Typography variant='h5' className=' p-3'>
+                        This extensive experience combining creative solutions with effective communication and organization allows me to:
+                    </Typography>
 
-                    <li id='intro-li-1'>
+                    <li id='intro-li-1' className='mb-2 pixelify-sans'>
                         Keep up in fast-paced work environments
                     </li>
 
-                    <li id='intro-li-2'>
+                    <li id='intro-li-2' className='mb-2 pixelify-sans'>
                         Handles stressful situations with the end goal always in mind
                     </li>
 
-                    <li id='intro-li-3'>
+                    <li id='intro-li-3' className='mb-2 pixelify-sans'>
                         Quickly adapt to cutting edge technologies
                     </li>
 
-                    <li id='intro-li-4'>
+                    <li id='intro-li-4' className='mb-2 pixelify-sans'>
                         Approach each problem with a combination of logical understanding and creative flair
                     </li>
                 </ul>
@@ -41,7 +47,7 @@ export default function About() {
 
             </div>
 
-            <div className='container exp-container'>
+            <div className='container exp-container mt-5'>
                 <ScrollAnimation animateIn="fadeIn" duration={2} animateOnce={true}>
                     <Experience />
                 </ScrollAnimation>
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,6 +5,7 @@ export default function Resume() {
                 <a href='/pdfs/Morgan_Clarke_Resume.pdf' download='Morgan_Clarke_Resume.pdf'>
                     {/* <h3 id="resumeText">Download My Resume</h3> */}
                     <img id='resumePic' src='/pngs/resume.png' alt='Morgan Clarke Resume' className='border rounded-4 shadow-lg p-5 m-5' />
+                    <div id="resumeText">Download My Resume</div>
                 </a>
             </div>
         </>
